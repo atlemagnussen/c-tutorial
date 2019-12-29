79ea7057dbd71e1e4d9fc26fa890ea9ec9c8d315
Atle Magnussen <atlemagnussen@gmail.com>
2019-12-29 10:33:29 +0100

    link multiple libs with cmake

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include "../config.h"
+#include "functions.h"
+#include "morefunctions.h"
+
 int main() {
     printf("name: %s\n", PROJECT_NAME);
     printf("version: %s\n", PROJECT_VERSION);
+    print_big(10);
+    print_hello("world");
     return 0;
 }
 
---
+++ src/morefunctions.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "morefunctions.h"
+
+void print_hello(char world[])
+{
+    printf("hello %s\n", world);
+}
