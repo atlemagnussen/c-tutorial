bd865c3cac7926702b86f4c5c11fa9fff5c81708
Atle Magnussen <atlemagnussen@gmail.com>
2019-12-29 09:50:15 +0100

    rename to build_gtk3

--- build_gkt3.sh
+++ build_gkt3.sh
@@ -1,5 +1,3 @@
 #!/bin/env bash
-#cc `pkg-config --cflags gtk+-3.0` -o example-0 example-0.c `pkg-config --libs gtk+-3.0`
 cc `pkg-config --cflags gtk+-3.0` -o example-1 example-1.c `pkg-config --libs gtk+-3.0`
 
-echo "built"
