9aac5d1cefcb5cf220644f6a734d7aef8a73f4d8
Atle Magnussen <atlemagnussen@gmail.com>
2020-01-01 17:26:50 +0100

    gitignore

--- gtk/build_gkt3.sh
+++ gtk/build_gkt3.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-cc `pkg-config --cflags gtk+-3.0` -o tralc-test test2.c `pkg-config --libs gtk+-3.0`
+cc `pkg-config --cflags gtk+-3.0` -o test2 test2.c `pkg-config --libs gtk+-3.0`
 
